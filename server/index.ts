8f5b295ae843315afd15a94f29461119844bc741
Imobach Martín <martin@365farmnet.com>
2025-02-15 22:57:54 +0000

    build: connect client with backend on dev environment

--- server/index.ts
+++ server/index.ts
@@ -5,6 +5,19 @@ async function main() {
   const app = express();
   app.use(express.json()); // Middleware to parse JSON bodies
 
+  if (process.env.ENV !== 'production') {
+    app.use((req, res, next) => {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT, PATCH');
+      res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+      next();
+    });
+  }
+
+  app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+
   const PORT = process.env.PORT || 3000;
   const server = app.listen(PORT);
 
