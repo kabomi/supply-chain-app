022890e81c59f0a0169480bec8562f2e0be030fd
Imobach Martín <martin@365farmnet.com>
2025-02-15 22:22:22 +0000

    build: change client output dir from dist to build

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // https://rollupjs.org/configuration-options/
+    outDir: 'build',
+  }
 })
