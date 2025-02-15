6544be75e60faf4d6f496315af5b3d6dfab8a714
Imobach Martín <martin@365farmnet.com>
2025-02-15 16:15:27 +0000

    First commit. Add simple server with ts support

---
+++ server/index.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json()); // Middleware to parse JSON bodies
+
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
