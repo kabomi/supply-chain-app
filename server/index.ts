39aa267ad57f68b6b42bbcbee387778807a8ac8c
Imobach Martín <martin@365farmnet.com>
2025-02-15 22:49:13 +0000

    feat: handle server errors. Fix nodemon crashing on npm run dev

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,48 @@
 import express from 'express';
+import logger from './logger';
 
-const app = express();
-app.use(express.json()); // Middleware to parse JSON bodies
+async function main() {
+  const app = express();
+  app.use(express.json()); // Middleware to parse JSON bodies
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
-});
+  const PORT = process.env.PORT || 3000;
+  const server = app.listen(PORT);
+
+  process.on('unhandledRejection', (reason) => {
+    logger.error('Unhandled Rejection at: Promise ', { reason });
+  });
+
+  process.on('uncaughtException', (error, origin) => {
+    if (origin === 'uncaughtException') {
+      logger.error('Uncaught Exception:', { error });
+      if (error.message.includes('EADDRINUSE')) {
+        logger.error('Port is already in use');
+        process.exit(1);
+      }
+    }
+  });
+
+  // process.on('SIGUSR2', function () {
+  //   console.log('\nGracefully shutting down from SIGUSR2');
+  //   server.close();
+  //   console.log(`Close the server listening on ${PORT}`);
+  // });
+  process.on('SIGINT', function () {
+    console.log('\nGracefully shutting down from SIGINT (Ctrl-C)');
+    server.close();
+    console.log(`Close the server listening on ${PORT}`);
+    process.exit(0);
+  });
+
+  server.on('listening', () => {
+    logger.log(`Server listening on ${PORT}`);
+  });
+
+  process.on('unhandledRejection', (reason) => {
+    logger.error('Unhandled Rejection at: Promise ', { reason });
+  });
+
+  return Promise.resolve(server);
+}
+
+export default main();
