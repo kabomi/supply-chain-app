7c3767516ab567bebd519052a4063b0f1faa4eff
Imobach Martín <martin@365farmnet.com>
2025-02-15 17:40:46 +0000

    config: add linter & prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,46 @@
+import prettier from 'eslint-plugin-prettier';
+import globals from 'globals';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+    {
+        ignores: ['node_modules', 'server/build'],
+    },
+  ...compat.extends('plugin:prettier/recommended'),
+  {
+    plugins: {
+      prettier,
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...Object.fromEntries(
+          Object.entries(globals.browser).map(([key]) => [key, 'off'])
+        ),
+        ...globals.jest,
+      },
+
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+
+    files: ["**/*.js", "**/*.ts", "**/*.mjs"],
+
+    rules: {
+        'no-console': 'off',
+        'prettier/prettier': 'error',
+    },
+  },
+];
