63790702fb28d80b595b20cdcf501e9de432fb62
Imobach Martín <martin@365farmnet.com>
2025-02-18 17:36:05 +0000

    build(client): Add vitest for client testing

---
+++ client/src/main.spec.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react';
+import { StrictMode } from 'react';
+import App from './App';
+
+test('renders App component without crashing', () => {
+  const { container } = render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+  expect(container).toBeDefined();
+});
+
+test('renders a specific element', () => {
+  render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+  const linkElement = screen.getByText(/Home/i);
+  expect(linkElement).toBeInTheDocument();
+});
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,10 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,5 +12,11 @@ export default defineConfig({
   build: {
     // https://rollupjs.org/configuration-options/
     outDir: 'build',
+  },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    css: true,
+    setupFiles: './src/vitest.setup.ts', // assuming the test folder is in the root of our project
   }
 })
